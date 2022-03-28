5d1f590cb208b1a90ade7c4a5529417db6ec072c
Mike <1079537183@qq.com>
2022-03-28 11:06:17 +0800

    deploy

---
+++ precache-manifest.2ce42aeac4453122c78478567b866bfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea99c5d6ef8c2cce7f79",
+    "url": "/tuanzi-tally-website-whole/css/app.24a6a801.css"
+  },
+  {
+    "revision": "42c7d59ae3b1b99e7f4f",
+    "url": "/tuanzi-tally-website-whole/css/chunk-vendors.1edc804b.css"
+  },
+  {
+    "revision": "70545a0e25f09b7e986b6b886189e78e",
+    "url": "/tuanzi-tally-website-whole/index.html"
+  },
+  {
+    "revision": "ea99c5d6ef8c2cce7f79",
+    "url": "/tuanzi-tally-website-whole/js/app.ba22dd83.js"
+  },
+  {
+    "revision": "42c7d59ae3b1b99e7f4f",
+    "url": "/tuanzi-tally-website-whole/js/chunk-vendors.d70164ef.js"
+  },
+  {
+    "revision": "9fe62da39c2aec4170812c9e5835c122",
+    "url": "/tuanzi-tally-website-whole/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tuanzi-tally-website-whole/robots.txt"
+  }
+]);
